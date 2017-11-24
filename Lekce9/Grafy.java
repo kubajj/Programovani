a987fe8a15c257689a1fb148b8fff5cd0183d30b
Kuba Jelínek <vilouskubajj@gmail.com>
2017-11-24 21:51:53 +0100

    Zjednoduseni kodu pro Grafy.java

--- Lekce9/Grafy.java
+++ Lekce9/Grafy.java
@@ -1,41 +1,37 @@
- public class Grafy{
+public class Grafy{
     public static void main(String args[]){
           double cisla[] = new double[100];
           System.out.printf("Zadejte maximalne 100 cisel:\n");
           java.util.Scanner sc = new java.util.Scanner(System.in);
           int pocet = 0;
-          for (int i = 0; i < 100; i++) {
-                if (!sc.hasNextDouble()) {
-                    break;
-                }
-                cisla[i] = sc.nextDouble();
-                pocet++;                
-          }
-          double cisladografu[] = new double[pocet];
-          for (int n = 0; n < pocet; n++){
-                cisladografu[n] = cisla[n];
-          }
-          boolean prvni = true;
+	  if (!sc.hasNextDouble()) {
+	      System.out.printf("Nezadal jste zadne cislo.\n");
+	      return;
+	  }
+	  while (sc.hasNextDouble() && pocet < 100) {
+                cisla[pocet++] = sc.nextDouble();
+	  }
+	  boolean prvni = true;
           double maximum = 0;
-          for (int i = 0; i < cisladografu.length; i++){
+          for (int i = 0; i < pocet; i++){
                   if (prvni) {
-                    maximum = cisladografu[i];
+                    maximum = cisla[i];
                     prvni = false;
                   } else {
-                  if (cisladografu[i] > maximum) {
-                    maximum = cisladografu[i];
+                  if (cisla[i] > maximum) {
+                    maximum = cisla[i];
                     }
                   }
           }
           double jednotka = 60./maximum;
-          System.out.printf("Maximum je %.2f, jednotka %.4f\n", maximum, jednotka);
-          for (int x = 0; x < cisladografu.length; x++){
-                    for (int a = 1; a <= cisladografu[x] * jednotka; a++ ){
-                          System.out.printf("#");
+          for (int x = 0; x < pocet; x++){
+	     for (int a = 1; a <= cisla[x] * jednotka; a++ ){
+                    System.out.printf("#");
                     
               }
               System.out.println();
           }
-          
     }
  }
+
+   
