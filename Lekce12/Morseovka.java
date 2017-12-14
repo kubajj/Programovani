38fdc503767bddf7d89fc20355128ed1f21462ae
student <student>
2017-12-14 16:01:50 +0100

    Lekce12

---
+++ Lekce12/Morseovka.java
@@ -0,0 +1,21 @@
+public class Morseovka{
+  public static void main(String args[]) {  
+    char jedna[] = new char[] {'A', 'E', 'G', 'J', 'K', 'N', 'Q', 'W', 'Y', '1','9'};
+    char dva[] = new char[] {'C', 'I', 'P', 'R', 'U', 'X', 'Z','2','8'} ;
+    char tri[] = new char[] {'B', 'F', 'L', 'S', 'V','3','7'};
+    char ctyri[] = new char[] {'H','4','6'};
+    char pet[]  = new char[] {'5'};
+    
+    java.util.Scanner sc = new java.util.Scanner(System.in);
+    String znak = " ";
+      while(sc.hasNext()){
+            znak = sc.hasNext();
+            if ()
+      }
+     
+    
+                                
+    
+    
+  }
+}
