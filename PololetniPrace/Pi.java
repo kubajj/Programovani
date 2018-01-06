26296958706ac0fcb0b2b47ed7abfd00da95538f
Kuba Jelínek <vilouskubajj@gmail.com>
2018-01-06 21:31:31 +0100

    Pridani Pi.java

--- PololetniPrace/Pi.java
+++ PololetniPrace/Pi.java
@@ -1,5 +1,25 @@
 public class Pi {
+    public static boolean jeVKruhu (int radek, int sloupec, int pocetcisel) {
+	boolean je;
+	if (Math.sqrt(radek * radek + sloupec * sloupec) <= pocetcisel) {
+	    je = true;
+	} else {
+	    je = false;
+	}
+	return je;
+    }
+    
     public static void main(String[] args) {
-	
+	int polomer = Integer.parseInt(args[0]);
+	double vkruhu = 0;
+	double ctverec = 4 * polomer * polomer;
+	for (int radek =  - polomer; radek < polomer; radek++) {
+	    for (int sloupec = - polomer; sloupec < polomer; sloupec++) {
+		if (jeVKruhu(radek, sloupec, polomer)) 
+		    vkruhu++;		
+	    }
+	}
+	double pi = (vkruhu * 4) / ctverec;
+	System.out.printf("%f\n", pi);
     }
 }
