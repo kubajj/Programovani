9f45c63c3aafcc5fbbe91e495e1eeaa8b8fb8e32
Kuba Jelínek <vilouskubajj@gmail.com>
2018-01-07 22:58:03 +0100

    Vyreseni Cetnosti slov

---
+++ Lekce13/CetnostSlov.java
@@ -0,0 +1,12 @@
+public class CetnostSlov {
+    public static void main (String args[]){    
+	Cetnosti cetnost = new Cetnosti();
+        System.out.printf("Zadejte vas text:\n");
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+	while (sc.hasNext()) 
+	    cetnost.zapis(sc.next());
+        String cetnosti[] = cetnost.seznamSlov();
+	for (int i = 0; i < cetnosti.length; i++)
+	    System.out.printf("%s jste zadal %d-krat.\n", cetnosti[i], cetnost.cetnost(cetnosti[i]));
+    }
+}
---
+++ Lekce13/Cetnosti.java
@@ -0,0 +1,39 @@
+public class Cetnosti {
+    private String pole[];
+    private int pocet;
+    public Cetnosti() { pocet = 0; }
+    public void zapis(String slovo) {
+	for (int i = 0; i < pocet; i += 2)
+	    if (pole[i].equals(slovo)) {
+		int cislo = Integer.parseInt(pole[i + 1]);
+		cislo++;
+		pole[i + 1] = "" + cislo;
+		return;
+	    }
+	if (pocet == 0)
+	    pole = new String[16];
+	else if (pole.length == pocet) {
+	    String polenove[] = new String[pole.length * 2];
+	    System.arraycopy(pole, 0, polenove, 0, pole.length);
+	    pole = polenove;
+	}
+	pole[pocet++] = slovo;
+	pole[pocet++] = "1";
+    }
+    
+    public String[] seznamSlov() {
+	String ret[] = new String[pocet / 2];
+	for (int i = 0; i < pocet / 2; i++)
+	    ret[i] = pole[2 * i];
+	return ret;
+    }
+    
+    public int cetnost(String slovo) {
+	for (int i = 0; i < pocet; i += 2)
+	    if (pole[i].equals(slovo)) {
+		int cislo = Integer.parseInt(pole[i + 1]);
+		return cislo;
+	    }
+	return 0;
+    }
+}
